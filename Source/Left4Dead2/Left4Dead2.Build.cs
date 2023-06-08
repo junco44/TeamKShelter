7e14a0d9000f009067146f19ead947630b384a68
junco44 <cjswnsgh44@gmail.com>
2023-06-08 16:43:50 +0900

    q

--- Source/Left4Dead2/Left4Dead2.Build.cs
+++ Source/Left4Dead2/Left4Dead2.Build.cs
@@ -8,7 +8,7 @@ public class Left4Dead2 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "OnlineSubsystemSteam", "OnlineSubsystem", "AIModule", "GameplayTasks", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
